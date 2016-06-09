27f9ff4a576da43656b48a786aa1842ebb9e836d
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-09 12:45:47 -0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "${var.provider_region}"
+}
+
+module "app" {
+  source = "../module-aws-app"
+
+  provider_region = "${var.provider_region}"
+
+  bucket_remote_state = "${var.bucket_remote_state}"
+  context_org = "${var.context_org}"
+  context_env = "${var.context_env}"
+
+  az_count = "${var.az_count}"
+}
+
+module "service" {
+  source = "../module-aws-service"
+
+  provider_region = "${var.provider_region}"
+
+  bucket_remote_state = "${var.bucket_remote_state}"
+  context_org = "${var.context_org}"
+  context_env = "${var.context_env}"
+
+  az_count = "${var.az_count}"
+
+  cidr_blocks = "${var.cidr_blocks}"
+}
