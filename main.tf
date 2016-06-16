9585c978edb78ec3a9b0f16b5cfe553cd693500a
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-15 23:35:53 -0700

    move to fogg

--- main.tf
+++ main.tf
@@ -1,33 +1,3 @@
-provider "aws" { }
-
-data "terraform_remote_state" "global" {
-  backend = "s3"
-  config {
-    bucket = "${var.bucket_remote_state}"
-    key = "${var.bucket_remote_state}/env-${var.context_org}-global.tfstate"
-  }
-}
-
-module "app" {
-  source = "../module-aws-app"
-
-  bucket_remote_state = "${var.bucket_remote_state}"
-  context_org = "${var.context_org}"
-  context_env = "${var.context_env}"
-}
-
-module "default" {
-  source = "../module-aws-service"
-
-  bucket_remote_state = "${var.bucket_remote_state}"
-  context_org = "${var.context_org}"
-  context_env = "${var.context_env}"
-
-  cidr_blocks = "${var.cidr_blocks}"
-
-  az_count = "${var.az_count}"
-}
-
 resource "aws_eip" "nat" {
   count = "${var.az_count}"
 
